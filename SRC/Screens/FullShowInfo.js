c892d5e37a52b6e3c78f0b0c52669c26a433f5f8
Aditya Kumar <adityakumar@Aditya-Kumars-Mac.local>
2020-07-23 16:50:42 +0530

    published

--- SRC/Screens/FullShowInfo.js
+++ SRC/Screens/FullShowInfo.js
@@ -1,183 +1,182 @@
 import React from "react"
 import {
-	View,
-	Text,
-	Image,
-	StyleSheet,
-	ImageBackground,
-	SafeAreaView,
-	StatusBar,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  StatusBar,
 } from "react-native"
 import { connect } from "react-redux"
 import {
-	FlatList,
-	TouchableOpacity,
-	ScrollView,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
 } from "react-native-gesture-handler"
 import { AntDesign } from "@expo/vector-icons"
 import { useNavigation } from "@react-navigation/native"
 
 class FullShowInfo extends React.Component {
-	render() {
-		var { selectedShow } = this.props
+  render() {
+    var { selectedShow } = this.props
 
-		return (
-			<View style={{ flex: 1 }}>
-				<StatusBar barStyle="light-content" />
-				<ImageBackground
-					style={fullStyle.fullImage}
-					source={{ uri: selectedShow.image.original }}
-				>
-					<SafeAreaView style={fullStyle.overlay}>
-						<View style={fullStyle.overlayContainer}>
-							<TouchableOpacity
-								onPress={() => {
-									this.props.Navigation.goBack()
-								}}
-								style={{ marginVertical: 10 }}
-							>
-								<AntDesign name="leftcircle" color="white" size={25} />
-							</TouchableOpacity>
-							<View
-								style={{
-									flexDirection: "row",
-									alignItems: "center",
-								}}
-							>
-								<Text
-									style={{
-										color: "white",
-										fontSize: 30,
-										fontWeight: "bold",
-										textAlign: "center",
-									}}
-								>
-									{selectedShow.name.toUpperCase()}
-								</Text>
-								<Text
-									style={{ color: "white", fontWeight: "200", fontSize: 20 }}
-								>
-									( {selectedShow.premiered.slice(0, 4)} )
-								</Text>
-							</View>
-							<View style={{ flexDirection: "row", marginBottom: 20 }}>
-								<FlatList
-									scrollEnabled={false}
-									horizontal
-									data={selectedShow.genres}
-									renderItem={(x) => {
-										return (
-											<Text style={{ color: "white", marginRight: 10 }}>
-												{x.item}
-											</Text>
-										)
-									}}
-									keyE
-									xtractor={(x, y) => {
-										return "KeyGenres-" + y
-									}}
-									ListFooterComponent={() => {
-										return (
-											<View
-												style={{
-													alignItems: "center",
-													flexDirection: "row",
-												}}
-											>
-												<View
-													style={{
-														backgroundColor: "white",
-														height: 5,
-														width: 5,
-														borderRadius: 5,
-														marginRight: 10,
-													}}
-												></View>
-												<Text style={{ color: "white" }}>
-													{selectedShow.runtime} min
-												</Text>
-											</View>
-										)
-									}}
-								/>
-							</View>
-							<View
-								style={{
-									flexDirection: "row",
-									alignItems: "center",
-									marginBottom: 20,
-								}}
-							>
-								<View
-									style={{
-										borderColor: "lightgreen",
-										width: 55,
-										height: 55,
-										alignItems: "center",
-										justifyContent: "center",
-										borderRadius: 30,
-										borderWidth: 3,
-									}}
-								>
-									<Text style={{ color: "white", fontSize: 15 }}>
-										{selectedShow.rating.average}/10
-									</Text>
-								</View>
-								<View style={{ marginLeft: 10 }}>
-									<Text style={{ color: "white", fontWeight: "bold" }}>
-										User
-									</Text>
-									<Text style={{ color: "white", fontWeight: "bold" }}>
-										Score
-									</Text>
-								</View>
-							</View>
-							<View
-								style={{
-									alignSelf: "flex-start",
-									borderBottomColor: "lightskyblue",
-									borderBottomWidth: 2,
-								}}
-							>
-								<Text style={{ color: "white", fontSize: 24 }}>Overview</Text>
-							</View>
-							<ScrollView>
-								<Text style={{ color: "white", fontSize: 18, marginTop: 15 }}>
-									{selectedShow.summary.replace(/p>|b>|i>|<|[/]/g, "")}
-								</Text>
-							</ScrollView>
-						</View>
-					</SafeAreaView>
-				</ImageBackground>
-			</View>
-		)
-	}
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          style={fullStyle.fullImage}
+          source={{ uri: selectedShow.image.original }}
+        >
+          <SafeAreaView style={fullStyle.overlay}>
+            <View style={fullStyle.overlayContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.Navigation.goBack()
+                }}
+                style={{ marginVertical: 10 }}
+              >
+                <AntDesign name="leftcircle" color="white" size={25} />
+              </TouchableOpacity>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: 30,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                  }}
+                >
+                  {selectedShow.name.toUpperCase()}
+                </Text>
+                <Text
+                  style={{ color: "white", fontWeight: "200", fontSize: 20 }}
+                >
+                  ( {selectedShow.premiered.slice(0, 4)} )
+                </Text>
+              </View>
+              <View style={{ flexDirection: "row", marginBottom: 20 }}>
+                <FlatList
+                  scrollEnabled={false}
+                  horizontal
+                  data={selectedShow.genres}
+                  renderItem={(x) => {
+                    return (
+                      <Text style={{ color: "white", marginRight: 10 }}>
+                        {x.item}
+                      </Text>
+                    )
+                  }}
+                  keyExtractor={(x, y) => {
+                    return "KeyGenres-" + y
+                  }}
+                  ListFooterComponent={() => {
+                    return (
+                      <View
+                        style={{
+                          alignItems: "center",
+                          flexDirection: "row",
+                        }}
+                      >
+                        <View
+                          style={{
+                            backgroundColor: "white",
+                            height: 5,
+                            width: 5,
+                            borderRadius: 5,
+                            marginRight: 10,
+                          }}
+                        ></View>
+                        <Text style={{ color: "white" }}>
+                          {selectedShow.runtime} min
+                        </Text>
+                      </View>
+                    )
+                  }}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginBottom: 20,
+                }}
+              >
+                <View
+                  style={{
+                    borderColor: "lightgreen",
+                    width: 55,
+                    height: 55,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    borderWidth: 3,
+                  }}
+                >
+                  <Text style={{ color: "white", fontSize: 15 }}>
+                    {selectedShow.rating.average}/10
+                  </Text>
+                </View>
+                <View style={{ marginLeft: 10 }}>
+                  <Text style={{ color: "white", fontWeight: "bold" }}>
+                    User
+                  </Text>
+                  <Text style={{ color: "white", fontWeight: "bold" }}>
+                    Score
+                  </Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  alignSelf: "flex-start",
+                  borderBottomColor: "lightskyblue",
+                  borderBottomWidth: 2,
+                }}
+              >
+                <Text style={{ color: "white", fontSize: 24 }}>Overview</Text>
+              </View>
+              <ScrollView>
+                <Text style={{ color: "white", fontSize: 18, marginTop: 15 }}>
+                  {selectedShow.summary.replace(/p>|b>|i>|<|[/]/g, "")}
+                </Text>
+              </ScrollView>
+            </View>
+          </SafeAreaView>
+        </ImageBackground>
+      </View>
+    )
+  }
 }
 
 var fullStyle = StyleSheet.create({
-	fullImage: {
-		width: "100%",
-		height: "100%",
-	},
-	overlay: {
-		backgroundColor: "rgba(0,0,0,0.7)",
-		flex: 1,
-		alignItems: "center",
-	},
-	overlayContainer: {
-		height: "100%",
-		width: "95%",
-	},
+  fullImage: {
+    width: "100%",
+    height: "100%",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.7)",
+    flex: 1,
+    alignItems: "center",
+  },
+  overlayContainer: {
+    height: "100%",
+    width: "95%",
+  },
 })
 
 var mapStateToProps = (state) => {
-	return state
+  return state
 }
 
 var fullshowFunction = (props) => {
-	var Navigation = useNavigation()
+  var Navigation = useNavigation()
 
-	return <FullShowInfo {...props} Navigation={Navigation} />
+  return <FullShowInfo {...props} Navigation={Navigation} />
 }
 
 export default connect(mapStateToProps)(fullshowFunction)
--- SRC/Screens/HomePage.js
+++ SRC/Screens/HomePage.js
@@ -8,156 +8,157 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import { AntDesign } from "@expo/vector-icons"
 
 class HomePage extends React.Component {
-	componentDidMount() {
-		this.props.getdata()
-	}
+  componentDidMount() {
+    this.props.getdata()
+  }
 
-	componentDidUpdate(preProp) {
-		if (preProp.allMovieData !== this.props.allMovieData) {
-			if (
-				this.props.bestShows.length === 0 &&
-				this.props.comedyShows.length === 0 &&
-				this.props.animatedShows.length === 0
-			) {
-				this.props.allMovieData.map((show, index, arr) => {
-					if (show.rating.average >= 9) {
-						this.props.getBest(show)
-					}
+  componentDidUpdate(preProp) {
+    if (preProp.allMovieData !== this.props.allMovieData) {
+      if (
+        this.props.bestShows.length === 0 &&
+        this.props.comedyShows.length === 0 &&
+        this.props.animatedShows.length === 0
+      ) {
+        this.props.allMovieData.map((show, index, arr) => {
+          if (show.rating.average >= 9) {
+            this.props.getBest(show)
+          }
 
-					// if (index === arr.length - 1) {
-					// 	console.log("Done")
-					// 	console.log(this.state.bestDone)
-					// }
+          // if (index === arr.length - 1) {
+          // 	console.log("Done")
+          // 	console.log(this.state.bestDone)
+          // }
 
-					show.genres.map((gen) => {
-						if (gen === "Comedy") {
-							this.props.getComedy(show)
-						}
-					})
-					if (show.type === "Animation") {
-						var x = 0
+          show.genres.map((gen) => {
+            if (gen === "Comedy") {
+              this.props.getComedy(show)
+            }
+          })
+          if (show.type === "Animation") {
+            var x = 0
 
-						show.genres.map((gen, index, arr) => {
-							if (gen === "Anime") {
-								x = 1
-							}
-							if (index === arr.length - 1) {
-								if (x === 0) {
-									this.props.getAnimation(show)
-								}
-							}
-						})
-					}
-				})
-			}
-		}
-	}
+            show.genres.map((gen, index, arr) => {
+              if (gen === "Anime") {
+                x = 1
+              }
+              if (index === arr.length - 1) {
+                if (x === 0) {
+                  this.props.getAnimation(show)
+                }
+              }
+            })
+          }
+        })
+      }
+    }
+  }
 
-	renderThis = () => {
-		if (
-			this.props.bestShows.length > 0 &&
-			this.props.comedyShows.length > 0 &&
-			this.props.animatedShows.length > 0
-		) {
-			return (
-				<>
-					<MovieView
-						whatToSearchProp={this.props.bestShows}
-						headNameProp="Most Popular"
-					/>
-					<MovieView
-						whatToSearchProp={this.props.comedyShows}
-						headNameProp="Comedy"
-					/>
+  renderThis = () => {
+    if (
+      this.props.bestShows.length > 0 &&
+      this.props.comedyShows.length > 0 &&
+      this.props.animatedShows.length > 0
+    ) {
+      return (
+        <>
+          <MovieView
+            whatToSearchProp={this.props.bestShows}
+            headNameProp="Most Popular"
+          />
+          <MovieView
+            whatToSearchProp={this.props.comedyShows}
+            headNameProp="Comedy"
+          />
 
-					<MovieView
-						whatToSearchProp={this.props.animatedShows}
-						headNameProp="Animation"
-					/>
-				</>
-			)
-		} else {
-			return (
-				<View
-					style={{
-						alignItems: "center",
-						justifyContent: "center",
-						height: "100%",
-					}}
-				>
-					<Text>Loading...</Text>
-				</View>
-			)
-		}
-	}
+          <MovieView
+            whatToSearchProp={this.props.animatedShows}
+            headNameProp="Animation"
+          />
+        </>
+      )
+    } else {
+      return (
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            backgroundColor: "black",
+          }}
+        >
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+  }
 
-	render() {
-		return (
-			<SafeAreaView style={{ backgroundColor: "black" }}>
-				<StatusBar barStyle="light-content" />
+  render() {
+    return (
+      <SafeAreaView style={{ backgroundColor: "black" }}>
+        <StatusBar barStyle="light-content" />
 
-				<Text
-					style={{
-						color: "white",
-						fontWeight: "bold",
-						alignSelf: "center",
-						fontSize: 65,
-						letterSpacing: 20,
-						width: "90%",
-						transform: [{ translateX: 10 }],
-					}}
-				>
-					SERIES
-				</Text>
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            alignSelf: "center",
+            fontSize: 65,
+            letterSpacing: 20,
+            width: "90%",
+            transform: [{ translateX: 10 }],
+          }}
+        >
+          SERIES
+        </Text>
 
-				<ScrollView showsVerticalScrollIndicator={false}>
-					<View
-						style={{
-							height: 50,
-							flexDirection: "row",
-							alignItems: "center",
-							justifyContent: "space-between",
-							marginVertical: 10,
-						}}
-					>
-						<TextInput
-							style={{
-								height: "70%",
-								width: "80%",
-								backgroundColor: "white",
-								marginLeft: 10,
-								borderRadius: 25,
-								paddingHorizontal: 10,
-							}}
-						/>
-						<TouchableOpacity
-							style={{
-								backgroundColor: "lightskyblue",
-								height: 35,
-								width: 35,
-								borderRadius: 50,
-								alignItems: "center",
-								justifyContent: "center",
-								marginRight: 10,
-							}}
-						>
-							<AntDesign name="search1" color="white" size={17} />
-						</TouchableOpacity>
-					</View>
-					{this.renderThis()}
-				</ScrollView>
-			</SafeAreaView>
-		)
-	}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{
+              height: 50,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginVertical: 10,
+            }}
+          >
+            <TextInput
+              style={{
+                height: "70%",
+                width: "80%",
+                backgroundColor: "white",
+                marginLeft: 10,
+                borderRadius: 25,
+                paddingHorizontal: 10,
+              }}
+            />
+            <TouchableOpacity
+              style={{
+                backgroundColor: "lightskyblue",
+                height: 35,
+                width: 35,
+                borderRadius: 50,
+                alignItems: "center",
+                justifyContent: "center",
+                marginRight: 10,
+              }}
+            >
+              <AntDesign name="search1" color="white" size={17} />
+            </TouchableOpacity>
+          </View>
+          {this.renderThis()}
+        </ScrollView>
+      </SafeAreaView>
+    )
+  }
 }
 
 var mapStateToProps = (state) => {
-	return state
+  return state
 }
 
 export default connect(mapStateToProps, {
-	getdata: getdata,
-	getBest: getBest,
-	getComedy: getComedy,
-	getAnimation: getAnimation,
+  getdata: getdata,
+  getBest: getBest,
+  getComedy: getComedy,
+  getAnimation: getAnimation,
 })(HomePage)
